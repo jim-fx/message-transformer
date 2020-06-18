a604262275171dca566695d41d48ed925649be09
mrichte8 <max.richter@smail.th-koeln.de>
2020-06-18 14:11:26 +0200

    important change

---
+++ README.md
@@ -0,0 +1,3 @@
+# anton-saphire-v2
+
+This is the repo for the prototype of the anton-saphire-v2-machine for the course social hubs.
---
+++ static/input/app.js
@@ -0,0 +1,57 @@
+var socket = io();
+var inputElement = document.getElementById("text-input");
+var startListenElement = document.getElementById("start-listening");
+
+var typeWriter = new Typewriter('#typeWrapper', {
+  autoStart: true,
+  devMode: false,
+  delay: 35,
+});
+
+function sendMessage(msg) {
+  socket.emit("msg", msg);
+  writeToScreen("sending: " + msg);
+
+  setTimeout(() => {
+    writeToScreen("message sent")
+  })
+}
+
+socket.on("msg", console.log);
+
+var speech = new webkitSpeechRecognition();
+
+speech.continuous = true;
+
+startListenElement.addEventListener("click", function () {
+  speech.start();
+  this.style.display = "none";
+  writeToScreen("Listening to you . . . . . . .")
+});
+
+var currentResultIndex = 0;
+speech.onresult = function (event) {
+  console.log(event);
+  var res = event.results[currentResultIndex][0].transcript;
+  currentResultIndex++;
+  sendMessage(res);
+};
+
+// inputElement.addEventListener("keydown", function (ev) {
+//   if (ev.keyCode === 13) {
+//     sendMessage(inputElement.value);
+//     inputElement.value = "";
+//   }
+// });
+
+function writeToScreen(msg) {
+  var now = new Date();
+  var date = now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + "  |  " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+
+  typeWriter.typeString("<br>" + date + " ==> " + msg + "<br>");
+  typeWriter.start();
+
+  //document.body.innerHTML += "<br>" + date +" --> " + msg + "<br>";
+
+
+}
